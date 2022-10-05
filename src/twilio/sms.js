d41591017995303432d49b2556f90fc3dcc4dccf
francocornejo <franco-cornejo@hotmail.com>
2022-10-05 20:14:58 -0300

    error en twilio, solucion temporal

--- src/twilio/sms.js
+++ src/twilio/sms.js
@@ -1,7 +1,7 @@
 import twilio from 'twilio'
 
-const accountSid = process.env.ACCOUNT_SID
-const authToken = process.env.ACCOUNT_TOKEN
+const accountSid = "AC947712e1e902fff59a68af1b3a1342e8"//process.env.ACCOUNT_SID 
+const authToken = "609defe7b5cc48028a591636e7273a00"//process.env.ACCOUNT_TOKEN
 
 const client = twilio(accountSid, authToken)
 
@@ -12,7 +12,7 @@ export default async function envioSms(to,body) {
         to: `+54${to}`
       },(err, info)=>{
           if (err) {
-              console.log(err)
+              console.log("Este Error es de mensajes: ", err)
           } else {
               console.log('sms sent: ', info);
           }
--- src/twilio/whatsapp.js
+++ src/twilio/whatsapp.js
@@ -1,7 +1,7 @@
 import twilio from 'twilio'
 
-const accountSid = process.env.ACCOUNT_SID 
-const authToken = process.env.ACCOUNT_TOKEN
+const accountSid = "AC947712e1e902fff59a68af1b3a1342e8"//process.env.ACCOUNT_SID 
+const authToken = "609defe7b5cc48028a591636e7273a00"//process.env.ACCOUNT_TOKEN
 
 const client = twilio(accountSid, authToken)
 
@@ -12,7 +12,7 @@ export default async function mainWhatsapp(body) {
        to: "whatsapp:+5491140330618"
      },(err, info)=>{
          if (err) {
-             console.log(err)
+             console.log("Este Error es de whatsapp: ", err)
          } else {
              console.log('whatsapp sent: ', info);
          }
